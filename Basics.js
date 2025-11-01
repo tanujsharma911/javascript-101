1fd3e06e0d5f72a415933e449a3c3fe7d22f138f
Tanuj Sharma <hellotanujsharma@gmail.com>
2025-11-01 17:50:36 +0530

    first commit

---
+++ Basics.js
@@ -0,0 +1,14 @@
+"use strict";  // use modern javascript syntax
+
+let var1 = 10;
+let var2 = 20;
+let var3 = 30;
+
+console.log("Hello world");
+console.table([var1, var2, var3]); // prints values in table format
+
+// Colors
+console.log('\x1b[31m%s\x1b[0m', 'This is a red line');
+console.log('\x1b[32m%s\x1b[0m', 'This is a green line');
+console.log('\x1b[34m%s\x1b[0m', 'This is a blue line');
+console.log('\x1b[33m%s\x1b[0m', 'This is a yellow line');
